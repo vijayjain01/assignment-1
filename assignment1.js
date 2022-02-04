522fdf9625091e4dae3c95d4ea8e910437c4599e
vijayjain01 <98397527+vijayjain01@users.noreply.github.com>
2022-02-04 11:52:42 +0530

    Assignment 1

---
+++ assignment1.js
@@ -0,0 +1,14 @@
+let a = 20;
+let b = 10;
+console.log(a+b);
+console.log(a-b);
+
+let sum = 60;
+let sub = 30;
+let mult = 10;
+let div = 20;
+console.log(Number(sum+sub));
+console.log(Number(sum-sub));
+console.log(Number(sub*mult));
+console.log(Number(sum/div));
+console.log(Number(sum%div));
